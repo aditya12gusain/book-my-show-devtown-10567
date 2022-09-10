7c9883adb3d42a2b516bb3d9ef79fdc508468690
aditya12gusain <adisandi3112002@gmail.com>
2022-09-10 21:27:37 +0530

    Initial Setup for project Completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// React slick css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
